3beb1842bf2f4bbbb43cbbbbc69fc003fc0a3385
mehmetsimseknet <2.me.hts@gmail.com>
2022-07-05 17:00:40 +0300

    added createReport.html

--- src/main/java/com/project/ozguryazilim/controllers/homePageController.java
+++ src/main/java/com/project/ozguryazilim/controllers/homePageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class homePageController {
     
     @GetMapping("/")
-    public String signIn(){return "user/signIn";}
+    public String signIn(){return "report/createReport";}
 
 }
 
